72e85bf36f97838377146bf98e68eac9263f59a0
tttsaurus <chenh1231y@gmail.com>
2025-02-27 17:10:41 -0500

    Updated config desc

--- src/main/java/com/tttsaurus/fluxloading/FluxLoadingConfig.java
+++ src/main/java/com/tttsaurus/fluxloading/FluxLoadingConfig.java
@@ -19,7 +19,7 @@ public class FluxLoadingConfig
         {
             CONFIG.load();
 
-            WAIT_CHUNK_BUILD_COEFFICIENT = CONFIG.getFloat("Wait Chunk Build Coefficient", "general", 1f, 0f, 1f, "Theoretically, FluxLoading will wait until all chunks finished building if \"Wait Chunk Build Coefficient\" equaled 1.0\nSet it to lower numbers even 0.0 if you stuck at the loading screen");
+            WAIT_CHUNK_BUILD_COEFFICIENT = CONFIG.getFloat("Wait Chunk Build Coefficient", "general", 1f, 0f, 2f, "FluxLoading estimates the number of chunks to build and waits until that stage\nYou can adjust this coefficient to modify the chunk loading wait time\nA value of 1.0 works for normal render distance");
             EXTRA_WAIT_TIME = CONFIG.getFloat("Extra Wait Time", "general", 0.5f, 0.1f, 10f, "Extra wait time before fade out animation");
             FADE_OUT_DURATION = CONFIG.getFloat("Fade Out Duration", "general", 1.0f, 0.5f, 10f, "The actual fade out time may feel shorter than this due to the exponentially decaying fade out function");
 
