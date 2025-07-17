b80eb7b5755af40c15eee96f84044bc742ccd087
tttsaurus <72607439+tttsaurus@users.noreply.github.com>
2025-07-17 13:37:07 +0800

    Make LoadingScreenRenderer proxy buffer static

--- src/main/java/com/tttsaurus/fluxloading/mixin/early/LoadingScreenRendererMixin.java
+++ src/main/java/com/tttsaurus/fluxloading/mixin/early/LoadingScreenRendererMixin.java
@@ -21,7 +21,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public class LoadingScreenRendererMixin
 {
     @Unique
-    private BufferBuilder fluxloading$proxyBufferBuilder;
+    private static BufferBuilder fluxloading$proxyBufferBuilder;
 
     @WrapOperation(
             method = "setLoadingProgress",
